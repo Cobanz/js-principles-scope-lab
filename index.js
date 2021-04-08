817869fb1e4205712746cb05a788273acb1797c3
Charles Grant Nichols <grant@inkdots.com>
2021-04-07 19:18:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+// const upperCaseCustomerName=() => {customerName.toUpperCase} 
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'hello';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Assignment to constant variable.';
+}
